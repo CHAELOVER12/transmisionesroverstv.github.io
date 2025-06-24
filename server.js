aa18ab6193fc786925508e7436cbf34523e0c81e
Aldair Grimaldo <gedgaraldair@gmail.com>
2025-06-23 23:39:18 -0600

    Configurando puerto para Railway

--- server.js
+++ server.js
@@ -3,14 +3,13 @@ const fs = require('fs');
 const { spawn } = require('child_process');
 const express = require('express');
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use('/replays', express.static('uploads'));
 app.use(express.static('public'));
 
-app.listen(PORT, () => console.log(`Servidor web escuchando en http://localhost:${PORT}`));
-
-const wss = new WebSocket.Server({ port: 3002 });
+const server = app.listen(PORT, () => console.log(`Servidor web escuchando en http://localhost:${PORT}`));
+const wss = new WebSocket.Server({ server });
 wss.on('connection', ws => {
   const nombreArchivo = `uploads/partido_${Date.now()}.mp4`;
   const ffmpeg = spawn('ffmpeg', ['-i', 'pipe:0', '-c:v', 'libx264', '-preset', 'veryfast', nombreArchivo]);
